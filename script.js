173943cf8232ea35bf04c51f6cc387fd647890d1
yatchanek <yatsu@gazeta.pl>
2020-11-21 13:51:02 +0100

    Lives, High Score, Gameover message

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const gameWindow = document.getElementById('game')
 const ctx = gameWindow.getContext('2d')
-const eggs = []
 
+let eggs = []
 let tickLength = 700
 let elapsedTime = 0
 let spawnInterval = 6
 let eggSpeed = Math.PI * 2
 let lastFrame = 0
 let ticks = 0
+let fails = 0
 let gameOver = false
 let score = 0
+let highScore = 0
 let lastEgg = 0
 let preLastEgg = 1
 let mouseX = 0
@@ -23,6 +25,7 @@ let slopeWidth = 20
 let upper = h * 0.15
 let bottom = h * 0.70
 
+
 class Egg {
     constructor(pos) {
         this.r = eggSize
@@ -30,28 +33,35 @@ class Egg {
         this.y = pos < 2 ? upper - this.r - slopeWidth / 2 : bottom - this.r - slopeWidth / 2
         this.pos = pos
         this.dir = pos % 2 === 0 ? 1 : -1
-        this.steps = 0
         this.rotationSpeed = eggSpeed
         this.angle = 0
         this.interval = (Math.PI * 2) / 50
         this.circ = this.r * Math.PI * 2
         this.dist = 0
+        this.lifetime = 1
+        this.expired = false
+        this.broken = false
     }
 
     update(delta) {
-        if (!this.caught) {
-            this.angle += this.rotationSpeed * delta
-            let d = this.circ * (this.rotationSpeed * delta / (Math.PI * 2)) * this.dir
-            this.x += d * Math.cos(slopeAngle)
-            this.y += Math.abs(d) * Math.sin(slopeAngle)
-            this.dist += Math.abs(d)
-            
+
+        this.angle += this.rotationSpeed * delta
+        let d = this.circ * (this.rotationSpeed * delta / (Math.PI * 2)) * this.dir
+        this.x += d * Math.cos(slopeAngle)
+        this.y += Math.abs(d) * Math.sin(slopeAngle)
+        this.dist += Math.abs(d)
+
+        if (this.broken) {
+            this.lifetime -= 4 * delta
+            if (this.lifetime <= 0) {
+                this.expired = true
+                this.lifetime = 0
+            }
+        }   
+        else {
             if (this.dist >= slopeLength * 1.1) {
-                if (this.pos != player.pos) {
-                    gameOver = true
-                }  
-                else {
-                    this.caught = true
+                if (this.pos === player.pos) {
+                    this.expired = true
                     score++
                     if (score > 0 && score % 10 === 0 && tickLength > 100) {
                         tickLength -=50
@@ -61,30 +71,37 @@ class Egg {
                     if (score > 0 && score % 25 === 0 && spawnInterval > 2) {
                         spawnInterval--
                     }
+                    if (score > highScore) {
+                        highScore = score
+                    }
+                }  
+                else if (this.dist >= slopeLength * 1.15) {
+                    fails++
+                    this.broken = true
+                    this.dist = 0
+                    if (fails === 4) {
+                        fails = 3
+                        gameOver = true
+                        localStorage.setItem('JajeczkaHighScore', highScore)
+                    } 
                 }
-            }
+            }           
         }
-        else {
-            this.expired = true
-        }
-
     }
 
     show() {
-        let i = 0
-        ctx.strokeStyle = `rgb(${i}, 0, 0)`
         ctx.lineWidth = 0
-            for(i = 0; i < 50; i++) {
-                ctx.fillStyle = `hsl(${360 * i / 50}, 65%, 50%)`
-                ctx.beginPath()
-                ctx.moveTo(this.x, this.y)
-                ctx.lineTo(this.x + this.r * Math.cos(i * this.interval + this.angle * this.dir), this.y + this.r * Math.sin(i * this.interval + this.angle * this.dir))
-                ctx.arc(this.x, this.y, this.r, i * this.interval + this.angle * this.dir, (i + 1) * this.interval + this.angle * this.dir)
-                ctx.moveTo(this.x + this.r * Math.cos((i+1) * this.interval + this.angle * this.dir), this.y + this.r * Math.sin((i+1) * this.interval + this.angle * this.dir))
-                ctx.lineTo(this.x, this.y)
-                ctx.closePath()
-                ctx.fill()
-            }
+        for(let i = 0; i < 50; i++) {
+            ctx.fillStyle = `hsl(${360 * i / 50}, 65%, 50%)`
+            ctx.beginPath()
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(this.x + this.r * Math.cos(i * this.interval + this.angle * this.dir), this.y + this.r * Math.sin(i * this.interval + this.angle * this.dir))
+            ctx.arc(this.x, this.y, this.r, i * this.interval + this.angle * this.dir, (i + this.lifetime) * this.interval + this.angle * this.dir)
+            ctx.moveTo(this.x + this.r * Math.cos((i + this.lifetime) * this.interval + this.angle * this.dir), this.y + this.r * Math.sin((i + this.lifetime) * this.interval + this.angle * this.dir))
+            ctx.lineTo(this.x, this.y)
+            ctx.closePath()
+            ctx.fill()
+        }
     }
 }
 
@@ -128,6 +145,7 @@ class Player {
 function drawField() {
     ctx.strokeStyle = 'rgb(115, 77, 38)'
     ctx.lineWidth = slopeWidth
+    ctx.lineCap = 'round'
 
     //Left up
     ctx.beginPath()
@@ -153,12 +171,11 @@ function drawField() {
     ctx.lineTo(gameWindow.width - slopeLength * Math.cos(slopeAngle), bottom + slopeLength * Math.sin(slopeAngle))
     ctx.stroke()
 
-    ctx.font = `${Math.floor(gameWindow.width * 0.02)}px Arial`
-    let txt = `Score: ${score}`
-    let tw = ctx.measureText(txt).width
-    ctx.baseline = 'middle'
+    ctx.font = `${Math.floor(w * 0.02)}px Arial`
+    let txt = `High Score: ${highScore}   Score: ${score}   Fails: ${fails}`
+    ctx.baseline = 'bottom'
     ctx.fillStyle = 'rgb(255, 255,255)'
-    ctx.fillText(txt, (gameWindow.width - tw) / 2, gameWindow.height / 2)
+    ctx.fillText(txt, 10, h- 10)
 }
 
 function spawnEgg() {
@@ -177,31 +194,30 @@ function cls() {
     ctx.fillRect(0,0, w, h)
 }
 
-function mouseInBounds(x1, y1, x2, y2) {
-    return (mouseX > x1 && mouseX < x2 && mouseY > y1 && mouseY < y2)
+function mouseInBounds(mx, my, x1, y1, x2, y2) {
+    return (mx > x1 && mx < x2 && my > y1 && my < y2)
 }
 
 function handleClick(e) {
-    mouseX = e.pageX
-    mouseY = e.pageY
-
-    if (mouseInBounds(0, 0, w / 5, h / 4)) {
+    if (mouseInBounds(e.pageX, e.pageY, 0, 0, w / 5, h / 4)) {
         player.pos = 0
     }
 
-
-    if (mouseInBounds(w * 0.8, 0, w, h / 4)) {
+    if (mouseInBounds(e.pageX, e.pageY, w * 0.8, 0, w, h / 4)) {
         player.pos = 1
     }
 
-    if (mouseInBounds(0, h * 0.75, w / 5, h)) {
+    if (mouseInBounds(e.pageX, e.pageY, 0, h * 0.75, w / 5, h)) {
         player.pos = 2
     }
 
-    if (mouseInBounds(w * 0.8, h * 0.75, w, h)) {
+    if (mouseInBounds(e.pageX, e.pageY, w * 0.8, h * 0.75, w, h)) {
         player.pos = 3
     }
 
+    if (gameOver) {
+        restart()
+    }
 }
 
 function handleKeyInput(e) {
@@ -211,6 +227,7 @@ function handleKeyInput(e) {
         break
 
         case 'e':
+        case 'p':
             player.pos = 1
         break
 
@@ -219,9 +236,23 @@ function handleKeyInput(e) {
         break
 
         case 'd':
+        case 'l':
             player.pos = 3
         break
     }
+}
+
+function restart() {
+    eggs = []
+    tickLength = 700
+    spawnInterval = 6
+    eggSpeed = Math.PI * 2
+    fails = 0
+    score = 0
+    ticks = 0
+    elapsedTime = 0
+    gameOver = false
+    requestAnimationFrame(gameLoop)
 }
 
 function gameLoop(timestamp) {
@@ -230,12 +261,12 @@ function gameLoop(timestamp) {
     lastFrame = timestamp
     elapsedTime += delta
     for (let i = eggs.length - 1; i>=0; i--) {
-                 eggs[i].update(delta / 1000)
-                 if (eggs[i].expired) {
-                     eggs.splice(i, 1)
-                 }
-         }
-     if (elapsedTime >= tickLength) {     
+        eggs[i].update(delta / 1000)
+        if (eggs[i].expired) {
+            eggs.splice(i, 1)
+        }
+    }
+    if (elapsedTime >= tickLength) {     
         elapsedTime = 0
         ticks++
         if (ticks % spawnInterval === 0) {
@@ -243,16 +274,24 @@ function gameLoop(timestamp) {
         }
      }
 
-
     drawField()
     eggs.forEach(e => e.show())
     player.show()
     
     if (!gameOver) requestAnimationFrame(gameLoop)
-
+    else {
+        let txt = 'Game Over'
+        ctx.font = `10vw Arial`
+        let tw = ctx.measureText(txt).width
+        ctx.baseline = 'middle'
+        ctx.fillText(txt, (w - tw) / 2, h / 2)
+    }
 }
 
 const player = new Player()
+if (localStorage.hasOwnProperty('JajeczkaHighScore')) {
+    highScore = localStorage.getItem('JajeczkaHighScore')
+}
 
 requestAnimationFrame(gameLoop)
 
